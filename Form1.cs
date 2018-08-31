7d63d3ea77697a167b8899651e9e5c786ddae932
joyce922 <42799563+joyce922@users.noreply.github.com>
2018-08-31 10:08:58 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] sıra = (textBox1.Text).Split(textBox2.Text.ToCharArray());
+
+            int[] a = Array.ConvertAll(sıra, s => int.Parse(s));
+
+            Array.Sort(a);
+
+            int temp = 0;
+            if (temp == 0)
+            {
+                textBox1.Clear();
+                foreach (int myInt in a)
+                {
+                    textBox1.AppendText(myInt.ToString() + textBox2.Text.ToString());
+                } 
+            }    
+        }
+    }
+}
